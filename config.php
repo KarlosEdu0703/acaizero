018a525ace4c29982980cf7878891f078813127f
Karlos <muffim.kat2010@gmail.com>
2026-05-18 21:36:24 -0300

    CU

--- config.php
+++ config.php
@@ -2,20 +2,14 @@
 $host = 'localhost';
 $db   = 'sabor_arte';
 $user = 'root';
-$pass = ''; // No XAMPP o padrão é vazio
-$charset = 'utf8mb4';
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
+$pass = '';
+$sgbd = 'mysql';
 
 try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (\PDOException $e) {
-    die("Erro na conexão: " . $e->getMessage());
+    $pdo = new PDO("$sgbd:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    die("Falha na conexão com o banco de dados: " . $e->getMessage());
 }
 ?>
-
